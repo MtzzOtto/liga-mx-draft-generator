87554d649656840dc44a32cdf7e1cdc47763e587
Otoniel <hackcheat159@gmail.com>
2026-05-26 19:15:24 -0600

    Adjust player selection

--- src/main.ts
+++ src/main.ts
@@ -145,6 +145,10 @@ if (app) {
     </label>
     <select id="playerSelect" text="Select number of players">
       <option value="">Select number of players</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
       <option value="5">5</option>
       <option value="6">6</option>
       <option value="7">7</option>
